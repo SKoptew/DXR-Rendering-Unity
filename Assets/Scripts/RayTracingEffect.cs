63c972850936b91479a80b76f1689ed53d22bec3
Sergey Koptev <s.koptev@corp.nekki.ru>
2021-03-25 01:17:45 +0300

    Hello raytracing. Miss shader: ray world dir, hit: black. Proper scene silhouette.

--- Assets/Scripts/RayTracingEffect.cs
+++ Assets/Scripts/RayTracingEffect.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
@@ -17,6 +16,20 @@ public class RayTracingEffect : MonoBehaviour
     private int _cameraWidth, 
                 _cameraHeight;
 
+    private static class ShaderID
+    {
+        //-- global
+
+        //-- local
+        public static readonly int _SceneAccelerationStructure = Shader.PropertyToID("_SceneAccelerationStructure");
+        public static readonly int _RenderTarget               = Shader.PropertyToID("_RenderTarget");
+        public static readonly int _InvViewMatrix              = Shader.PropertyToID("_InvViewMatrix");
+        public static readonly int _FrustumBottomLeftDirWS     = Shader.PropertyToID("_FrustumBottomLeftDirWS");
+        public static readonly int _FrustumHorizDirWS          = Shader.PropertyToID("_FrustumHorizDirWS");
+        public static readonly int _FrustumVertDirWS           = Shader.PropertyToID("_FrustumVertDirWS");
+    }
+    
+
     public void Update()
     {
         PrepareResources();
@@ -29,24 +42,45 @@ public class RayTracingEffect : MonoBehaviour
 
     public void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        if (!CheckResources())
+        var cam = Camera.main;
+        
+        if (!CheckResources(cam))
         {
             Graphics.Blit(src, dest);
             return;
         }
 
+        //-- shader pass "RTPass" in material shaders
         rtShader.SetShaderPass("RTPass");
         
-        //rtShader.SetVector...
+        //- set global variables, used in all material shaders
         
-        //rtShader.SetAccelerationStructure
+        //-- set local RT raygen shader variables
+        _rtAccStructure.Build();
+        rtShader.SetAccelerationStructure(ShaderID._SceneAccelerationStructure, _rtAccStructure);
+        
+        rtShader.SetTexture(ShaderID._RenderTarget,  _rtTargetTexture);
+        rtShader.SetMatrix( ShaderID._InvViewMatrix, cam.cameraToWorldMatrix);
+
+        var camOrigin = cam.transform.position;
+        var frustumBottomLeftDirWS  = (cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane)) - camOrigin).normalized;
+        var frustumBottomRightDirWS = (cam.ViewportToWorldPoint(new Vector3(1, 0, cam.nearClipPlane)) - camOrigin).normalized;
+        var frustumTopLeftDirWS     = (cam.ViewportToWorldPoint(new Vector3(0, 1, cam.nearClipPlane)) - camOrigin).normalized;
+
+        rtShader.SetVector(ShaderID._FrustumBottomLeftDirWS, frustumBottomLeftDirWS);
+        rtShader.SetVector(ShaderID._FrustumHorizDirWS, frustumBottomRightDirWS - frustumBottomLeftDirWS);
+        rtShader.SetVector(ShaderID._FrustumVertDirWS, frustumTopLeftDirWS - frustumBottomLeftDirWS);
+
+        //-- dispatch ray tracing
+        rtShader.Dispatch("RaygenShader", cam.pixelWidth, cam.pixelHeight, 1);
+        Graphics.Blit(_rtTargetTexture, dest);
     }
 
     private void PrepareResources()
     {
-        BuildAccelerationStructure();
+        CreateAccelerationStructure();
 
-        if (_cameraWidth != Camera.main.pixelWidth || _cameraHeight != Camera.main.pixelHeight)
+        if (_rtTargetTexture == null || _cameraWidth != Camera.main.pixelWidth || _cameraHeight != Camera.main.pixelHeight)
         {
             _cameraWidth  = Camera.main.pixelWidth;
             _cameraHeight = Camera.main.pixelHeight;
@@ -57,7 +91,7 @@ public class RayTracingEffect : MonoBehaviour
         }
     }
 
-    private void BuildAccelerationStructure()
+    private void CreateAccelerationStructure()
     {
         if (_rtAccStructure == null)
         {
@@ -85,7 +119,7 @@ public class RayTracingEffect : MonoBehaviour
         }
     }
 
-    private bool CheckResources()
+    private bool CheckResources(Camera cam)
     {
         if (!SystemInfo.supportsRayTracing)
         {
@@ -93,7 +127,7 @@ public class RayTracingEffect : MonoBehaviour
             return false;
         }
         
-        if (rtShader == null || _rtAccStructure == null)
+        if (cam == null || rtShader == null || _rtAccStructure == null)
             return false;
 
         return true;
